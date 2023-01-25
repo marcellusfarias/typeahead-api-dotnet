f6f72db8a67ecd4a3c9b92c822a62a2e100f43e3
Marcellus Farias <marcelluscfarias@gmail.com>
2023-01-25 18:29:35 -0300

    Added logging and starting Trie implementation. Needs testing

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Xml.Serialization;
+using log4net;
+public class Program
+{
+    private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+    static void Main(string[] args)
+    {
+        FileInfo fileInfo = new FileInfo("./log4net.config");
+        log4net.Config.XmlConfigurator.Configure(fileInfo);
+
+        log.Info("Starting service...");
+        Console.WriteLine("Hello World");
+        // Console.ReadLine();
+
+        //[TODO] Read configurations...
+        string host = string.Empty;
+        string fileName = string.Empty;
+        string port = string.Empty;
+        int suggestionNumber = 0;
+
+        try
+        {
+            string fileContent = File.ReadAllText(fileName);
+        }
+        catch (Exception)
+        {
+            log.Error("Could not load configuration environment!");
+            return;
+        }
+
+    }
+}
